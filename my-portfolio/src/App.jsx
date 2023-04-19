83c86923531fd7d9ed16511191f782d067ecc3f9
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2023-04-19 12:47:58 +0530

    Header section: Done

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+ 
+  return (
+    <div className="font-pop scroll-smooth">
+    <Header/>
+    </div>
+  )
+}
+
+export default App
